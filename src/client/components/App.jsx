f0209ce5a2c0e396f8894ac37d4fb0189c05659f
Vas2016 <vasily.yuryev.a1@gmail.com>
2018-02-22 00:32:34 +0700

    material-ui

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,11 +1,40 @@
 import React, {  Component } from 'react';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import Toggle from 'material-ui/Toggle';
+import DatePicker from 'material-ui/DatePicker';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import { red500, green500, green300, white, teal400, orange500, deepOrange500, amber500 } from 'material-ui/styles/colors';
+
+
+
 class App extends Component {
   render() {
+
     return (
-      <div className='App'>
-        <h1>Hello World!</h1>
-        
+      <div>
+        <MuiThemeProvider>
+          
+         
+          <div className="App" style={{
+            padding: 20
+          }}>
+            <RaisedButton label="Tester" backgroundColor={amber500} labelColor={white} style={{ margin: 5 }} />
+            <RaisedButton label="Tester" backgroundColor={orange500} labelColor={white} style={{ margin: 5 }} />
+            <RaisedButton label="Tester" backgroundColor={deepOrange500} labelColor={white} style={{ margin: 5 }} />
+            <RaisedButton label="Tester" backgroundColor={red500} labelColor={white} style={{ margin: 5 }} />
+            <RaisedButton label="Tean" backgroundColor={teal400} labelColor={white} style={{ margin: 5 }} />
+            <RaisedButton label="Green" backgroundColor={green500} labelColor={white} style={{ margin: 5 }} />
+            <Toggle label="Simple" />
+            <Toggle label="Simple" />
+            <DatePicker hintText="Landscape Dialog" mode="landscape" />
+            <FloatingActionButton zDepth={2} style={{ position: "fixed", right: 20, bottom: 20 }} backgroundColor={deepOrange500}>
+              <ContentAdd />
+            </FloatingActionButton>
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
