fa22870d809722542646869744ebf8bc011053fe
Vas2016 <vasily.yuryev.a1@gmail.com>
2018-02-22 00:23:52 +0700

    Update

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+console.log(__dirname)
+app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/public', express.static('../public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
