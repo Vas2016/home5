101c3866f25a10ceed460ba8ef2c905f9cba9390
Vas2016 <vasily.yuryev.a1@gmail.com>
2018-03-30 15:20:50 +0700

    get ip addr

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var g = require('./g.js')
 var ips = require('./utils/getLocalIP.js').getIP()
 console.log(ips)
 g.ip = ips[0]["ip"]
-console.log(require('./utils/getCPU.js').getCPUs())
+// console.log(require('./utils/getCPU.js').getCPUs())
 require('./server.js')
 require('./home5.js')
 require('./narodmon.js')
